58c4871e81f909f1e3d86bcfb1902bf52e353ca7
ehejia <ehejia@qq.com>
2019-11-28 18:10:57 +0800

    更新脚本

--- script/benchmark/tensorflow_banchmark.sh
+++ script/benchmark/tensorflow_banchmark.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 cd ~
-git clone https://github.com/tensorflow/benchmarks
-python ~/benchmarks/scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --batch_size=64 --model=resnet50 --num_gpus=1 --num_epochs=100
+git clone https://github.com/tensorflow/benchmarks && python3 ~/benchmarks/scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --batch_size=64 --model=resnet50 --num_gpus=1 --num_epochs=100
--- script/conda/conda_create_env.sh
+++ script/conda/conda_create_env.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-conda create -n tensorflow113test
+conda create -n tensorflow113test python=3.5
 conda activeate tensorflow113test
+conda install tensorflow=1.13
