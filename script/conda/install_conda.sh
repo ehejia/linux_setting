f5811bbb5f12476a50d870a4103290894d87f493
ehejia <ehejia@qq.com>
2019-11-28 17:49:51 +0800

    ·
    ·

--- script/conda/install_conda.sh
+++ script/conda/install_conda.sh
@@ -2,8 +2,5 @@
 wget https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
 sh Anaconda3-2019.10-Linux-x86_64.sh -b -p /usr/local/anaconda3
 rm -rf Anaconda3-2019.10-Linux-x86_64.sh
-conda create tensorflow113test
-conda activeate tensorflow113test
-cd ~
-git clone https://github.com/tensorflow/benchmarks
-python ~/benchmarks/scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --batch_size=64 --model=resnet50 --num_gpus=1 --num_epochs=100
+/usr/local/anaconda3/bin/conda init
+exit 
