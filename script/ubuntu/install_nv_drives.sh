2181749033fa2d91e9ce6da7ff7f3d1c687422f9
ihejia <ehejia@qq.com>
2021-03-03 18:06:34 +0800

    更换驱动版本与安装方式

--- script/ubuntu/install_nv_drives.sh
+++ script/ubuntu/install_nv_drives.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-apt update
+sudo -s apt update
 #ubuntu-drivers devices
-#apt install -y nvidia-driver-430
-wget https://cn.download.nvidia.com/XFree86/Linux-x86_64/455.23.04/NVIDIA-Linux-x86_64-455.23.04.run
-chmod +x NVIDIA-Linux-x86_64-455.23.04.run
-./NVIDIA-Linux-x86_64-455.23.04.run -no-opengl-files -no-x-check -Z -z
-update-initramfs -u
-rm -rf NVIDIA-Linux-x86_64-455.23.04.run
-read -p  "驱动安装完成，重启后生效,任意一键继续！"
-reboot
+apt install -y nvidia-driver-460
+#rm -rf NVIDIA-Linux-x86_64-455.23.04.run
+#wget https://cn.download.nvidia.com/XFree86/Linux-x86_64/455.23.04/NVIDIA-Linux-x86_64-455.23.04.run
+#chmod +x NVIDIA-Linux-x86_64-455.23.04.run
+#sudo -s ./NVIDIA-Linux-x86_64-455.23.04.run -no-opengl-files -no-x-check -Z -z -dkms
+#sudo -s update-initramfs -u
+#rm -rf NVIDIA-Linux-x86_64-455.23.04.run
+#read -p  "驱动安装完成，重启后生效,任意一键继续！"
+#sudo reboot
