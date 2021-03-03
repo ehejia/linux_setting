bf1f0d7899890f8460fc9628540985961c41e588
ihejia <ehejia@qq.com>
2021-03-03 09:14:46 +0800

    更新驱动版本适应3000系列显卡

--- script/ubuntu/install_nv_drives.sh
+++ script/ubuntu/install_nv_drives.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 apt update
 #ubuntu-drivers devices
-apt install -y nvidia-driver-430
+#apt install -y nvidia-driver-430
+wget https://cn.download.nvidia.com/XFree86/Linux-x86_64/455.23.04/NVIDIA-Linux-x86_64-455.23.04.run
+chmod +x NVIDIA-Linux-x86_64-455.23.04.run
+./NVIDIA-Linux-x86_64-455.23.04.run -no-opengl-files -no-x-check -Z -Z
+update-initramfs -u
+rm -rf NVIDIA-Linux-x86_64-455.23.04.run
 read -p  "驱动安装完成，重启后生效,任意一键继续！"
 reboot
