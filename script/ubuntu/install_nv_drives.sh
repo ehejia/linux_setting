8ae5398f51bae4855f05bab177011afacfb0d935
ihejia <ehejia@qq.com>
2021-03-03 09:20:32 +0800

    命令中的一些小问题

--- script/ubuntu/install_nv_drives.sh
+++ script/ubuntu/install_nv_drives.sh
@@ -4,7 +4,7 @@ apt update
 #apt install -y nvidia-driver-430
 wget https://cn.download.nvidia.com/XFree86/Linux-x86_64/455.23.04/NVIDIA-Linux-x86_64-455.23.04.run
 chmod +x NVIDIA-Linux-x86_64-455.23.04.run
-./NVIDIA-Linux-x86_64-455.23.04.run -no-opengl-files -no-x-check -Z -Z
+./NVIDIA-Linux-x86_64-455.23.04.run -no-opengl-files -no-x-check -Z -z
 update-initramfs -u
 rm -rf NVIDIA-Linux-x86_64-455.23.04.run
 read -p  "驱动安装完成，重启后生效,任意一键继续！"
